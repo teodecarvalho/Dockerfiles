700ee52dec5249baa34dad64c51a166f0ab525dd
teo <teo.decarvalho@gmail.com>
2019-03-27 11:39:55 -0300

    Added anaconda

---
+++ anaconda/start_anaconda.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -i -t -p 8888:8888 continuumio/anaconda /bin/bash -c "/opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser"
