f50a51d7591d6821204c14bd713414b0edae98e4
Teo <teo.decarvalho@gmail.com>
2019-05-09 17:43:10 -0300

    Added Script to setup gcloud server with docker and jupyter

---
+++ google_cloud_docker/setup_docker_and_jupyter.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install docker
+sudo apt-get -y install \
+apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
+
+
+# Install jupyter
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+export PATH=~/miniconda/bin:$PATH
+
+echo "export PATH=~/miniconda/bin:$PATH" >> ~/.bashrc
+
+conda install jupyter --yes
+
+
